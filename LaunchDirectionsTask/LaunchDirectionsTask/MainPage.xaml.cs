073ae9578626785faa6f27665541bd0927b3926a
Jukka <jukka.silvennoinen@nokia.com>
2013-03-04 11:49:42 +0200

    Added maps launchers examples

---
+++ LaunchDirectionsTask/LaunchDirectionsTask/MainPage.xaml.cs
@@ -0,0 +1,253 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Maps.Controls;
+using System.Diagnostics;
+using System.Windows.Controls.Primitives;
+using System.Collections.ObjectModel;
+using Microsoft.Phone.Maps.Services;
+
+using Microsoft.Phone.Tasks;
+using System.Device.Location;
+namespace LaunchDirectionsTask
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        bool draggingNow = false;
+        MapLayer markerLayer = null;
+        MapOverlay selectedMarker = null;
+        MapOverlay OriginMarker = null;
+        MapOverlay DestinationMarker = null;
+
+        ReverseGeocodeQuery geoRev = null;
+        bool DestinationRevGeoNow = false;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            System.Windows.Input.Touch.FrameReported += Touch_FrameReported;
+
+            map1.ZoomLevelChanged += map1_ZoomLevelChanged;
+
+            AddResultToMap(new GeoCoordinate(60.17040395, 24.94121572), new GeoCoordinate(60.29143956, 24.96187636));
+
+            geoRev = new ReverseGeocodeQuery();
+            geoRev.QueryCompleted += geoRev_QueryCompleted;
+        }
+
+        void Touch_FrameReported(object sender, TouchFrameEventArgs e)
+        {
+            if (draggingNow == true)
+            {
+                TouchPoint tp = e.GetPrimaryTouchPoint(map1);
+
+                if (tp.Action == TouchAction.Move && (selectedMarker != null))
+                {
+                    selectedMarker.GeoCoordinate = map1.ConvertViewportPointToGeoCoordinate(tp.Position);
+                    Start_ReverceGeoCoding(selectedMarker);
+                }
+                else if (tp.Action == TouchAction.Up)
+                {
+                    selectedMarker = null;
+                    draggingNow = false;
+                    map1.IsEnabled = true;
+                }
+            }
+        }
+
+        void map1_ZoomLevelChanged(object sender, MapZoomLevelChangedEventArgs e)
+        {
+            zoomSlider.Value = map1.ZoomLevel;
+        }
+
+        private void zoomSlider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (zoomSlider != null)
+            {
+                map1.ZoomLevel = zoomSlider.Value;
+            }
+        }
+
+        private void AddResultToMap(GeoCoordinate origin, GeoCoordinate destination)
+        {
+            if (markerLayer != null)
+            {
+                map1.Layers.Remove(markerLayer);
+                markerLayer = null;
+            }
+
+            OriginMarker = MakeDotMarker(origin, false);
+            DestinationMarker = MakeDotMarker(destination, true);
+
+            markerLayer = new MapLayer();
+            map1.Layers.Add(markerLayer);
+            markerLayer.Add(OriginMarker);
+            markerLayer.Add(DestinationMarker);
+        }
+
+        private MapOverlay MakeDotMarker(GeoCoordinate location, bool isDestination)
+        {
+            MapOverlay Marker = new MapOverlay();
+
+            Marker.GeoCoordinate = location;
+
+            Ellipse Circhegraphic = new Ellipse();
+            if (isDestination == true)
+            {
+                Circhegraphic.Fill = new SolidColorBrush(Colors.Green);
+                Circhegraphic.Stroke = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Purple);
+            }
+            else
+            {
+                Circhegraphic.Fill = new SolidColorBrush(Colors.Yellow);
+                Circhegraphic.Stroke = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Red);
+            }
+
+            Circhegraphic.StrokeThickness = 30;
+            Circhegraphic.Opacity = 0.8;
+            Circhegraphic.Height = 80;
+            Circhegraphic.Width = 80;
+
+            Marker.Content = Circhegraphic;
+
+            Marker.PositionOrigin = new Point(0.5, 0.5);
+            Circhegraphic.MouseLeftButtonDown += textt_MouseLeftButtonDown;
+
+            return Marker;
+        }
+
+
+        void textt_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse clickedOne = sender as Ellipse;
+            if (clickedOne != null && OriginMarker != null && DestinationMarker != null)
+            {
+                Debug.WriteLine("Circhegraphic_MouseLeftButtonUp");
+
+                if (OriginMarker.Content == clickedOne)
+                {
+                    selectedMarker = OriginMarker;
+                    draggingNow = true;
+                    map1.IsEnabled = false;
+                }
+                else if (DestinationMarker.Content == clickedOne)
+                {
+                    selectedMarker = DestinationMarker;
+                    draggingNow = true;
+                    map1.IsEnabled = false;
+                }
+            }
+        }
+
+        void Start_ReverceGeoCoding(MapOverlay Marker)
+        {
+            if (geoRev.IsBusy != true && (Marker != null))
+            {
+                if (Marker == DestinationMarker)
+                {
+                    DestinationRevGeoNow = true;
+                    DestinationTitle.Text = "";
+                    GeoProgress2.IsEnabled = true;
+                    GeoProgress2.IsIndeterminate = true;
+                }
+                else
+                {
+                    DestinationRevGeoNow = false;
+                    OriginTitle.Text = "";
+                    GeoProgress1.IsEnabled = true;
+                    GeoProgress1.IsIndeterminate = true;
+                }
+
+                // Set the geo coordinate for the query
+                geoRev.GeoCoordinate = Marker.GeoCoordinate;
+                geoRev.QueryAsync();
+                Debug.WriteLine("RevGeocodeAsync started for location: ");
+            }
+        }
+
+        void geoRev_QueryCompleted(object sender, QueryCompletedEventArgs<IList<MapLocation>> e)
+        {
+            GeoProgress1.IsEnabled = false;
+            GeoProgress1.IsIndeterminate = false;
+            GeoProgress2.IsEnabled = false;
+            GeoProgress2.IsIndeterminate = false;
+
+            Debug.WriteLine("Geo query, error: " + e.Error);
+            Debug.WriteLine("Geo query, cancelled: " + e.Cancelled);
+            Debug.WriteLine("Geo query, cancelled: " + e.UserState.ToString());
+            Debug.WriteLine("Geo query, Result.Count(): " + e.Result.Count());
+
+            String GeoStuff = "";
+
+            if (e.Result.Count() > 0)
+            {
+                if (e.Result[0].Information.Address.Street.Length > 0)
+                {
+                    GeoStuff = GeoStuff + e.Result[0].Information.Address.Street;
+
+                    if (e.Result[0].Information.Address.HouseNumber.Length > 0)
+                    {
+                        GeoStuff = GeoStuff + " " + e.Result[0].Information.Address.HouseNumber;
+                    }
+                }
+
+                if (e.Result[0].Information.Address.City.Length > 0)
+                {
+                    if (GeoStuff.Length > 0)
+                    {
+                        GeoStuff = GeoStuff + ",";
+                    }
+
+                    GeoStuff = GeoStuff + " " + e.Result[0].Information.Address.City;
+
+                    if (e.Result[0].Information.Address.Country.Length > 0)
+                    {
+                        GeoStuff = GeoStuff + " " + e.Result[0].Information.Address.Country;
+                    }
+                }
+                else if (e.Result[0].Information.Address.Country.Length > 0)
+                {
+                    if (GeoStuff.Length > 0)
+                    {
+                        GeoStuff = GeoStuff + ",";
+                    }
+                    GeoStuff = GeoStuff + " " + e.Result[0].Information.Address.Country;
+                }
+            }
+            if (DestinationRevGeoNow == true)
+            {
+                DestinationTitle.Text = GeoStuff;
+            }
+            else
+            {
+                OriginTitle.Text = GeoStuff;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (sender == Rutte)
+            {
+                MapsDirectionsTask mapsDirectionsTask = new MapsDirectionsTask();
+
+                mapsDirectionsTask.Start = new LabeledMapLocation(OriginTitle.Text, OriginMarker.GeoCoordinate);
+                mapsDirectionsTask.End = new LabeledMapLocation(DestinationTitle.Text, DestinationMarker.GeoCoordinate);
+ 
+                mapsDirectionsTask.Show();
+            }
+        }
+    }
+}
---
+++ LaunchMapTask/LaunchMapTask/MainPage.xaml.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Maps.Controls;
+using System.Diagnostics;
+using System.Windows.Controls.Primitives;
+using System.Collections.ObjectModel;
+using Microsoft.Phone.Maps.Services;
+
+using Microsoft.Phone.Tasks;
+using System.Device.Location;
+
+namespace LaunchMapTask
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        GeoCoordinateWatcher watcher = null;
+        GeoCoordinate myPosition = null;
+        MapPolygon PolyCircle = null;
+
+        bool draggingNow = false;
+        MapLayer markerLayer = null; 
+        MapOverlay oneMarker = null;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            markerLayer = new MapLayer();
+            map1.Layers.Add(markerLayer);
+
+            System.Windows.Input.Touch.FrameReported += Touch_FrameReported;
+
+            map1.Tap += map1_Tap;
+            map1.ZoomLevelChanged += map1_ZoomLevelChanged;
+
+            watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.Default);
+            watcher.MovementThreshold = 20; // 20 meters
+
+            watcher.StatusChanged += new EventHandler<GeoPositionStatusChangedEventArgs>(OnStatusChanged);
+            watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(OnPositionChanged);
+
+            watcher.Start();
+
+            AddResultToMap(map1.Center);
+            zoomSlider.Value = map1.ZoomLevel;  
+        }
+
+        void SetMarkerLocation(GeoCoordinate newCoordinate)
+        {
+            oneMarker.GeoCoordinate = newCoordinate;
+        }
+
+        void map1_ZoomLevelChanged(object sender, MapZoomLevelChangedEventArgs e)
+        {
+            zoomSlider.Value = map1.ZoomLevel;
+        }
+
+        void map1_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            if (oneMarker != null)
+            {
+                Point markPoint = map1.ConvertGeoCoordinateToViewportPoint(oneMarker.GeoCoordinate);
+
+                if ((markPoint.X < 0 || markPoint.Y < 0)
+                || (map1.ActualWidth < markPoint.X) || (map1.ActualHeight < markPoint.Y))
+                {
+                    // tap event when we do not have the marker visible, so lets move it here
+                    SetMarkerLocation(map1.ConvertViewportPointToGeoCoordinate(e.GetPosition(map1)));
+                }
+            }
+        }
+
+        void Touch_FrameReported(object sender, TouchFrameEventArgs e)
+        {
+            if (draggingNow == true)
+            {
+                TouchPoint tp = e.GetPrimaryTouchPoint(map1);
+
+                if (tp.Action == TouchAction.Move)
+                {
+                    if (oneMarker != null)
+                    {
+                        SetMarkerLocation(map1.ConvertViewportPointToGeoCoordinate(tp.Position));
+                    }
+                }
+                else if (tp.Action == TouchAction.Up)
+                {
+                    draggingNow = false;
+                    map1.IsEnabled = true;
+                }
+            }
+        }
+
+        void textt_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (oneMarker != null)
+            {
+                draggingNow = true;
+                map1.IsEnabled = false;
+            }
+        }
+
+        private void AddResultToMap(GeoCoordinate location)
+        {
+            if (markerLayer != null)
+            {
+                map1.Layers.Remove(markerLayer);
+                markerLayer = null;
+            }
+
+            markerLayer = new MapLayer();
+            map1.Layers.Add(markerLayer);
+
+            oneMarker = new MapOverlay();
+            SetMarkerLocation(location);
+
+            Ellipse Circhegraphic = new Ellipse();
+            Circhegraphic.Fill = new SolidColorBrush(Colors.Yellow);
+            Circhegraphic.Stroke = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Red);
+            Circhegraphic.StrokeThickness = 30;
+            Circhegraphic.Opacity = 0.8;
+            Circhegraphic.Height = 80;
+            Circhegraphic.Width = 80;
+
+            oneMarker.Content = Circhegraphic;
+
+            oneMarker.PositionOrigin = new Point(0.5, 0.5);
+            Circhegraphic.MouseLeftButtonDown += textt_MouseLeftButtonDown;
+
+            markerLayer.Add(oneMarker);
+
+            map1.Center = oneMarker.GeoCoordinate;
+        }
+
+        
+
+
+        private void SearchTerm_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox txtbox = (sender as TextBox);
+            if (txtbox != null && (txtbox.Text.Length > 0))
+            {
+                LaunchButton.IsEnabled = true;
+            }
+            else
+            {
+                LaunchButton.IsEnabled = false;
+            }
+        }
+
+        private void zoomSlider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (zoomSlider != null)
+            {
+                map1.ZoomLevel = zoomSlider.Value;
+            }
+        }
+
+        void OnPositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            myPosition = e.Position.Location;
+            MylocDot.Fill = new SolidColorBrush(Colors.Green);
+            UpDateMyPositionCircle();
+        }
+
+        void OnStatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
+        {
+            switch (e.Status)
+            {
+                case GeoPositionStatus.Ready:
+                    break;
+                case GeoPositionStatus.Disabled:
+                case GeoPositionStatus.Initializing:
+                case GeoPositionStatus.NoData:
+                default:
+                    {
+                        myPosition = null;
+                        MylocDot.Fill = new SolidColorBrush(Colors.Gray);
+                        UpDateMyPositionCircle();
+                    }
+                    break;
+            }
+        }
+
+        void UpDateMyPositionCircle()
+        {
+            if (myPosition != null)
+            {
+                double accuracy = myPosition.HorizontalAccuracy;
+
+                if (accuracy < myPosition.VerticalAccuracy)
+                {
+                    accuracy = myPosition.VerticalAccuracy;
+                }
+
+                if (PolyCircle == null)
+                {
+                    PolyCircle = new MapPolygon();
+
+                    PolyCircle.FillColor = Color.FromArgb(0x55, 0x00, 0xFF, 0x00);
+                    PolyCircle.StrokeColor = Color.FromArgb(0xFF, 0x00, 0xFF, 0x00);
+                    PolyCircle.StrokeThickness = 1;
+
+                    map1.MapElements.Add(PolyCircle);
+                }
+
+                if (accuracy < 50)
+                {
+                    accuracy = 50; // to be able to show the polygon
+                }
+
+                PolyCircle.Path = CreateCircle(myPosition, accuracy);
+            }
+            else if (PolyCircle != null)
+            {
+                map1.MapElements.Remove(PolyCircle);
+                PolyCircle = null;
+            }
+        }
+
+        public static double ToRadian(double degrees)
+        {
+            return degrees * (Math.PI / 180);
+        }
+
+        public static double ToDegrees(double radians)
+        {
+            return radians * (180 / Math.PI);
+        }
+
+        public static GeoCoordinateCollection CreateCircle(GeoCoordinate center, double radius)
+        {
+            var earthRadius = 6367000; // radius in meters
+            var lat = ToRadian(center.Latitude); //radians
+            var lng = ToRadian(center.Longitude); //radians
+            var d = radius / earthRadius; // d = angular distance covered on earth's surface
+            var locations = new GeoCoordinateCollection();
+
+            for (var x = 0; x <= 360; x++)
+            {
+                var brng = ToRadian(x);
+                var latRadians = Math.Asin(Math.Sin(lat) * Math.Cos(d) + Math.Cos(lat) * Math.Sin(d) * Math.Cos(brng));
+                var lngRadians = lng + Math.Atan2(Math.Sin(brng) * Math.Sin(d) * Math.Cos(lat), Math.Cos(d) - Math.Sin(lat) * Math.Sin(latRadians));
+
+                locations.Add(new GeoCoordinate(ToDegrees(latRadians), ToDegrees(lngRadians)));
+            }
+
+            return locations;
+        }
+
+        void myLocation_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (myPosition != null)
+            {
+                map1.Center = myPosition;
+            }
+        }
+
+        private void Button_gridbut_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == LaunchButton)
+            {
+                MapsTask mapsTask = new MapsTask();
+
+                //You could Omit the Center property to use the user's current location.
+                mapsTask.Center = oneMarker.GeoCoordinate;
+                mapsTask.ZoomLevel = zoomSlider.Value;
+
+                mapsTask.Show();
+            }
+        }
+    }
+}
