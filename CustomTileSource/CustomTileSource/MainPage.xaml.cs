f238b45c673d77bc8c9566df11c2ff5179f72d10
Lucian Tomuta <lucian.tomuta@gmail.com>
2013-05-30 20:04:55 +0300

    Custom tile source example
    Example showing how a new tile source can be added to the map control.

---
+++ CustomTileSource/CustomTileSource/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using CustomTileSource.Resources;
+using Microsoft.Phone.Maps.Controls;
+
+namespace CustomTileSource
+{
+    public class MyCustomTileSource : TileSource
+    {
+        public MyCustomTileSource()
+        {
+
+        }
+
+        public override Uri GetUri(int x, int y, int zoomLevel)
+        {
+            string url = string.Format("http://tiles.kartat.kapsi.fi/ortokuva/{0}/{1}/{2}.jpg", zoomLevel, x, y);
+            System.Diagnostics.Debug.WriteLine(url);
+
+            return new Uri(url, UriKind.Absolute);
+        }
+    }
+
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
